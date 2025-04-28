fd80c286d35b1905890873a31cfbc41ead160b25
AOV1337 <aov.alejandro03@gmail.com>
2025-04-28 21:14:09 +0300

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,6 @@
 #include "Dog.h"
 #include "Cat.h"
 #include "Cow.h"
-#include "Tiger.h"
-#include "Snake.h"
-#include "Horse.h"
 
 using namespace std;
 
@@ -16,14 +13,11 @@ int main() {
     animals.push_back(make_unique<Dog>());
     animals.push_back(make_unique<Cat>());
     animals.push_back(make_unique<Cow>());
-    animals.push_back(make_unique<Tiger>());
-    animals.push_back(make_unique<Snake>());
-    animals.push_back(make_unique<Horse>());
 
     for (const auto& animal : animals) {
         animal->makeSound();
     }
-
+    
     system("pause");
 
     return 0;
